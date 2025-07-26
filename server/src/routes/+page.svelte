d3ac92154a321f5504e16690ec317dc2719a356e
uladzimir <01myit@gmail.com>
2025-07-26 16:38:30 +0300

    TOGGLE and DELETE realized

--- server/src/routes/+page.svelte
+++ server/src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+// @ts-nocheck
+
+
 	import { enhance } from '$app/forms';
 	import { invalidateAll } from '$app/navigation';
 
     let {data} = $props()
-    $inspect(data)
+    // $inspect(data)
 </script>
 
 <h1>Todos (server.ts)</h1>
@@ -14,14 +17,38 @@
             invalidateAll()
         }
     }}>
+        <input type="hidden" name="marker" value="ADD" />
         <input type="text" name="todo" />
     </form>
     <ul>
         {#each data.content.content as todo, i (todo.id) }
             <li>
-                <input type="checkbox">
+
+                <!-- svelte-ignore a11y_click_events_have_key_events -->
+                <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+                <form method=POST action=/api/todos
+                onclick={(e) => {if(e.target.type === 'checkbox') e.target.form.requestSubmit()}}
+                use:enhance={() => {
+                    return async ({update}) => {
+                        await update()
+                        invalidateAll()
+                    }
+                }} >
+                    <input type="hidden" name="marker" value="TOGGLE" />
+                    <input type="hidden" name="id" value={todo.id} />
+                    <input type="checkbox" name="done" checked={todo.done}/>
+                </form>
                 <span>{todo.text}</span>
-                <button>delete</button>
+                <form method=POST action=/api/todos use:enhance={() => {
+                    return async ({update}) => {
+                        await update()
+                        invalidateAll()
+                    }
+                }}>
+                    <input type="hidden" name="marker" value="DELETE" />
+                    <input type="hidden" name="deleteId" value={todo.id} />
+                    <button type="submit">delete</button>
+                </form>
             </li>
         {/each}
     </ul>
--- server/src/routes/api/todos/+server.ts
+++ server/src/routes/api/todos/+server.ts
@@ -1,14 +1,57 @@
-import { json, redirect } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit';
 import { file } from '$lib/server/api/file.js';
-export async function POST({ request }) {
-	const formData = await request.formData();
-	const todo = formData.get('todo');
-	try {
-		const db = await file.readFile('db/db.json');
-		if (db.ok) {
-			db.content = [...db.content, { id: Date.now(), text: todo, done: false }];
-			await file.writeFile('db/db.json', db.content);
-		}
-	} catch (error) {}
-	return json(todo);
+export async function POST ( { request } )
+{
+	const formData = await request.formData()
+	const marker = formData.get('marker')
+	switch ( marker )
+	{
+		case 'ADD':
+			const todo = formData.get('todo');
+			try {
+				const db = await file.readFile('db/db.json');
+				if (db.ok) {
+					db.content = [...db.content, { id: Date.now(), text: todo, done: false }];
+					await file.writeFile('db/db.json', db.content);
+				}
+			} catch ( error )
+			{
+				throw {ok: false, error}
+			}
+			return json( {ok: true, todo} );
+		case 'TOGGLE':
+			const id = formData.get('id');
+			const done = formData.get('done');
+			try {
+				let { ok, content } = await file.readFile( 'db/db.json' );
+				if (ok) {
+					content = content.map((todo: any) => {
+						if (todo.id === Number(id)) {
+							done === 'on' ? todo.done = true : todo.done = false
+						}
+						return todo;
+					});
+					await file.writeFile('db/db.json', content);
+				}
+			} catch ( error )
+			{
+				throw {ok: false, error}
+			}
+			return json( { ok: true, id } );
+		case 'DELETE':
+			const deleteId = formData.get('deleteId');
+			try {
+				let { ok, content } = await file.readFile( 'db/db.json' );
+				if (ok) {
+					content = content.filter((todo: any) => todo.id !== Number(deleteId));
+					await file.writeFile('db/db.json', content);
+				}
+			} catch ( error )
+			{
+				throw {ok: false, error}
+			}
+			return json( { ok: true, deleteId } );
+		default:
+			return json( {ok: false, error: 'Unknown marker'} );
+	}
 }
